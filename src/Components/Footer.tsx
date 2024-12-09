3dd7d5f1384255b34c54591c86f7015448161a9a
moshaosama <mohamedOSFekry@gmail.com>
2024-12-09 16:37:58 +0200

    Edit sideBar

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,9 @@
 import { FaArrowCircleUp } from "react-icons/fa";
 
 const Footer = () => {
+  function handleClick() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
   return (
     <>
       <div className="bg-[#212529] p-10 flex justify-center max-sm:justify-between items-center">
@@ -8,7 +11,10 @@ const Footer = () => {
           Copyright & Copy :{" "}
           <span className="text-[#ffc107]">Mohamed Osama</span>
         </h1>
-        <div className="absolute right-10 cursor-pointer ">
+        <div
+          className="absolute right-10 cursor-pointer "
+          onClick={() => handleClick()}
+        >
           <FaArrowCircleUp className="text-blue-500 text-5xl hover:text-blue-400 max-sm:text-3xl" />
         </div>
       </div>
