90b32873eaa30506b51fad3e15f4b3024e17f784
moshaosama <mohamedOSFekry@gmail.com>
2024-12-09 16:35:36 +0200

    Edit sideBar

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,13 +3,13 @@ import { FaArrowCircleUp } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <div className="bg-[#212529] p-10 flex justify-center items-center">
-        <h1 className="text-white text-xl">
+      <div className="bg-[#212529] p-10 flex justify-center max-sm:justify-between items-center">
+        <h1 className="text-white text-xl max-sm:text-sm">
           Copyright & Copy :{" "}
           <span className="text-[#ffc107]">Mohamed Osama</span>
         </h1>
         <div className="absolute right-10 cursor-pointer ">
-          <FaArrowCircleUp className="text-blue-500 text-5xl hover:text-blue-400" />
+          <FaArrowCircleUp className="text-blue-500 text-5xl hover:text-blue-400 max-sm:text-3xl" />
         </div>
       </div>
     </>
